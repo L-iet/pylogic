52bf1f579fdf695b2efca9f2992e4594c35a69eb
L-iet <joshjohnmark@gmail.com>
2024-04-16 21:31:28 -0700

    begin session. add code for not.

--- pylogic/proposition/not_.py
+++ pylogic/proposition/not_.py
@@ -1,12 +1,34 @@
+from __future__ import annotations
 from pylogic.proposition.proposition import Proposition
+from typing import TYPE_CHECKING, TypeVar, Generic
+
+TProposition = TypeVar("TProposition", bound="Proposition")
 
 
-class Not(Proposition):
-    def __init__(self, negated: Proposition, is_assumption: bool = False) -> None:
-        self.negated = negated
+def neg(
+    p: TProposition | Not[TProposition], is_assumption: bool = False
+) -> Not[TProposition] | TProposition:
+    if isinstance(p, Not):
+        return p.negated
+    return Not(p, is_assumption)
+
+
+class Not(Proposition, Generic[TProposition]):
+    def __init__(
+        self,
+        negated: TProposition,
+        is_assumption: bool = False,
+        _is_proven: bool = False,
+    ) -> None:
+        self.negated: TProposition = negated
         name = rf"~{negated.name}"
-        super().__init__(name, is_assumption)
+        super().__init__(name, is_assumption, _is_proven=_is_proven)
         self.is_atomic = False
 
+    def __eq__(self, other: Proposition) -> bool:
+        if isinstance(other, Not):
+            return other.negated == self.negated
+        return False
+
     def copy(self) -> "Not":
-        return Not(self.negated.copy(), self.is_assumption)
+        return Not(self.negated.copy(), self.is_assumption, _is_proven=self.is_proven)
